9336fcca183f01711bdd8b4dfd2ebdf4a4415f41
Jacinto Gomez <jgomez@rodacity.com>
2015-08-02 19:26:03 +0200

    Gestion de errores

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -16,19 +16,19 @@ exports.load = function (req, res, next, quizId){
 //GET /quizes
 exports.index = function (req, res){
 	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index.ejs', {quizes: quizes});
+		res.render('quizes/index.ejs', {quizes: quizes, errors: [] });
 	}).catch(function(error){next(error);});
 };
 
 //GET /quizes/new
 exports.new = function (req, res){
 	var quiz = models.Quiz.build({pregunta: "Pregunta", respuesta: "Respuesta"});
-	res.render('quizes/new', {quiz: quiz});
+	res.render('quizes/new', {quiz: quiz, errors: [] });
 };
 
 //GET /quizes/:id
 exports.show = function (req, res){
-	res.render('quizes/show', {quiz: req.quiz});
+	res.render('quizes/show', {quiz: req.quiz, errors: [] });
 };
 
 //GET /quizes/question
@@ -44,14 +44,21 @@ exports.answer = function (req, res){
 	if(req.query.respuesta === req.quiz.respuesta){
 		resultado = 'Correcto';
 	}
-	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: [] });
 };
 
 //POST /quizes/create
 exports.create = function (req, res){
 	var quiz = models.Quiz.build(req.body.quiz);
 	
-	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-		res.redirect('/quizes');
-	}) //Redirección HTTP (URL relativo) lista de preguntas
+	quiz.validate().then(function(err){
+		if(err){
+			console.log("Error: " + err.errors);
+			res.render('quizes/new', { quiz: quiz, errors: err.errors});
+		} else {
+			quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+				res.redirect('/quizes');
+			});
+		}
+	}); //Redirección HTTP (URL relativo) lista de preguntas
 };

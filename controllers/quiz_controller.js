aa2a4bd76f4fcc24d8636cb440ae10d87715aece
Jacinto Gomez <jgomez@rodacity.com>
2015-08-03 21:20:31 +0200

    Solución modulo 8

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -22,7 +22,7 @@ exports.index = function (req, res){
 
 //GET /quizes/new
 exports.new = function (req, res){
-	var quiz = models.Quiz.build({pregunta: "Pregunta", respuesta: "Respuesta"});
+	var quiz = models.Quiz.build({pregunta: "Pregunta", respuesta: "Respuesta", tema: "tema"});
 	res.render('quizes/new', {quiz: quiz, errors: [] });
 };
 
@@ -62,7 +62,7 @@ exports.create = function (req, res){
 			console.log("Error: " + err.errors);
 			res.render('quizes/new', { quiz: quiz, errors: err.errors});
 		} else {
-			quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+			quiz.save({fields: ["pregunta", "respuesta", "tema"]}).then(function(){
 				res.redirect('/quizes');
 			});
 		}
@@ -73,12 +73,13 @@ exports.create = function (req, res){
 exports.update = function (req, res){
 	req.quiz.pregunta = req.body.quiz.pregunta;
 	req.quiz.respuesta = req.body.quiz.respuesta;
+	req.quiz.tema = req.body.quiz.tema;
 	
 	req.quiz.validate().then(function(err){
 		if(err){
 			res.render('quizes/new', { quiz: req.quiz, errors: err.errors});
 		} else {
-			req.quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+			req.quiz.save({fields: ["pregunta", "respuesta", "tema"]}).then(function(){
 				res.redirect('/quizes');
 			});
 		}

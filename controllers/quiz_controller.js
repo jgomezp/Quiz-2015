72219e51e792454a2ea6bfb435ef609fd3e2cab2
Jacinto Gomez <jgomez@rodacity.com>
2015-08-03 20:51:20 +0200

    Función editar

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -47,6 +47,12 @@ exports.answer = function (req, res){
 	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: [] });
 };
 
+//GET /quizes/:id/edit
+exports.edit = function (req, res){
+	var quiz = req.quiz; //autoload
+	res.render('quizes/edit', {quiz: quiz, errors: [] });
+};
+
 //POST /quizes/create
 exports.create = function (req, res){
 	var quiz = models.Quiz.build(req.body.quiz);
@@ -57,6 +63,22 @@ exports.create = function (req, res){
 			res.render('quizes/new', { quiz: quiz, errors: err.errors});
 		} else {
 			quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+				res.redirect('/quizes');
+			});
+		}
+	}); //Redirección HTTP (URL relativo) lista de preguntas
+};
+
+//PUT /quizes/:id
+exports.update = function (req, res){
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+	
+	req.quiz.validate().then(function(err){
+		if(err){
+			res.render('quizes/new', { quiz: req.quiz, errors: err.errors});
+		} else {
+			req.quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
 				res.redirect('/quizes');
 			});
 		}

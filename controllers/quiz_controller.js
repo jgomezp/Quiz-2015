ae29efbfc761eb81231aaeecb10af93a99468a38
Jacinto Gomez <jgomez@rodacity.com>
2015-08-03 20:59:53 +0200

    Función delete

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -84,3 +84,10 @@ exports.update = function (req, res){
 		}
 	}); //Redirección HTTP (URL relativo) lista de preguntas
 };
+
+//DELETE /quizes/:id
+exports.destroy = function (req, res){
+	req.quiz.destroy().then(function(){
+		res.redirect('/quizes');
+	}).catch(function(error){next(error);});
+};
